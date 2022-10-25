3cc76760f6a21b12a04dbe2807d34328335ab8c3
Haris Avdicevic <haris.avdi93@gmail.com>
2022-10-25 10:44:28 +0200

    Initial structure

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import io.javalin.Javalin;
+
+public class Main {
+    public static void main(String[] args) {
+        var app = Javalin.create(/*config*/)
+                .get("/", ctx -> ctx.result("Hello World"))
+                .start(7070);
+    }
+}
