bce7e245d6c1661940d8f229fa33c9e25ab837b3
Haris Avdicevic <haris.avdi93@gmail.com>
2022-10-28 10:51:44 +0200

    Removed unnecessary exception

--- src/main/java/services/CanvasService.java
+++ src/main/java/services/CanvasService.java
@@ -8,10 +8,6 @@ public class CanvasService {
 
     public static String[][] generateEmptyArray(int size) {
 
-        if (size <= 0){
-            throw new ArrayIndexOutOfBoundsException("A canvas can't be of size 0 or less. Try to pick a positive number.");
-        }
-
         String[][] colorArr = new String[size][(int) Math.floor(size * 1.5)];
 
         Arrays.stream(colorArr).forEach(x->Arrays.fill(x,"white"));
