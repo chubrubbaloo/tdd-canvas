5d2ee30e4c2478738d7f82ec6656d47cb1116ef3
fatima bouchtaoui <fatima.bouchtaoui@utb.ecutbildning.se>
2022-11-01 14:32:40 +0100

    Test for putPixel

--- src/main/java/services/CanvasService.java
+++ src/main/java/services/CanvasService.java
@@ -1,10 +1,13 @@
 package services;
 
+import entities.Pixel;
+import repositories.PixelRepository;
+
 import java.util.Arrays;
 
+
+
 public class CanvasService {
-
-
 
     public static String[][] generateEmptyArray(int size) {
 
@@ -14,4 +17,23 @@ public class CanvasService {
 
         return colorArr;
     }
+
+    private final PixelRepository repository;
+
+    public CanvasService (PixelRepository repository) { this.repository = repository;}
+
+    public Pixel putPixel(int x, int y, String color) throws Exception {
+
+        var pixel = new Pixel(x, y, color);
+        repository.updatePixel(pixel);
+
+        if(pixel.getColor() == null) {
+            throw new Exception();
+        }
+        return pixel;
+    }
 }
+
+
+
+
---
+++ src/test/java/services/CanvasServiceTest.java
@@ -0,0 +1,47 @@
+package services;
+
+import entities.Pixel;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import repositories.MemoryPixelRepository;
+
+public class CanvasServiceTest {
+
+    MemoryPixelRepository repository;
+    CanvasService service;
+
+
+    @BeforeEach
+    void setup() {
+        repository = Mockito.mock(MemoryPixelRepository.class);
+        service = new CanvasService(repository);
+    }
+
+    @Test
+    @DisplayName("Test if put pixel stores correctly in repository")
+    void putPixelTest() {
+        //given
+        Pixel finalPixel = new Pixel(1, 2, "white");
+
+        //when/then
+        var pixel = Assertions.assertDoesNotThrow(() -> service.putPixel(
+                        finalPixel.getX(),
+                        finalPixel.getY(),
+                        finalPixel.getColor()));
+        //then
+        Assertions.assertNotNull(pixel);
+        Mockito.verify(repository).updatePixel(pixel);
+    }
+
+    @Test
+    @DisplayName("Test if color is null")
+    void ifPixelColorIsNull(){
+        var pixel = new Pixel (1, 2, null);
+
+        Assertions.assertThrows(Exception.class, () -> service.putPixel(pixel.getX(), pixel.getY(), pixel.getColor()));
+
+    }
+}
