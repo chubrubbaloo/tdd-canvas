e50da573f0bf048ccd4cb627b3faf0f0fe88fee5
filipsjostrand <92868216+filipsjostrand@users.noreply.github.com>
2022-11-03 15:09:13 +0100

    Add populated-pixel-test GetPixelsServiceTest

--- src/test/java/services/GetPixelsServiceTest.java
+++ src/test/java/services/GetPixelsServiceTest.java
@@ -1,5 +1,6 @@
 package services;
 
+import entities.Pixel;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -64,6 +65,26 @@ public class GetPixelsServiceTest {
 
         //When
         var expected = CanvasService.generateEmptyArray(50);
+        System.out.println(Arrays.deepToString(service.getPixels()));
+
+        //Then
+        Assertions.assertEquals(Arrays.deepToString(expected), Arrays.deepToString(result));
+    }
+
+    @Test
+    @DisplayName("Get populated dummy-canvas")
+    void get_populated_canvas_from_repository() {
+        //Given
+        var pixelList = new ArrayList<Pixel>();
+        pixelList.add(new Pixel(0, 2, "green"));
+        pixelList.add(new Pixel(0, 1, "blue"));
+        Mockito.when(repository.getAllPixels()).thenReturn(pixelList);
+        var result = service.getPixels();
+
+        //When
+        var expected = CanvasService.generateEmptyArray(50);
+        expected[0][2] = "green";
+        expected[0][1] = "blue";
         System.out.println(Arrays.deepToString(service.getPixels()));
 
         //Then
